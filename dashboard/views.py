b6473659543361b00f986102d212c8784f0b8e4f
anirudh-mk <its.me.anirudh.mk@gmail.com>
2023-09-30 13:24:04 +0530

    feat:(CreateBulkUserAPI): response changed

--- dashboard/views.py
+++ dashboard/views.py
@@ -134,14 +134,27 @@ class CreateBulkGroupsAPI(APIView):
 
     def post(self, request):
         user_count = User.objects.all().count()
-        group_count = user_count // 6 + 1
+
+        if user_count % 6 != 0:
+
+            remaining_users = 6 - (user_count % 6)
+
+            return CustomResponse(
+                general_message=f"Need {remaining_users} more users to assign group"
+            ).get_failure_response()
+
+        group_count = user_count // 6
+
         groups = []
+
         for group_num in range(1, group_count):
+
             group = Group(
                 id=uuid.uuid4(),
                 title=f"group{group_num}",
                 created_at=DateTimeUtils.get_current_utc_time()
             )
+
             groups.append(group)
 
         Group.objects.bulk_create(groups)
@@ -157,6 +170,7 @@ class UserGroupLinkBulkCreateAPI(APIView):
     def post(self, request):
         users = list(User.objects.all())
         groups = list(Group.objects.all())
+
         try:
             with transaction.atomic():
                 for group in groups:
